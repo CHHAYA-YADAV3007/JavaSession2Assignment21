9671807547cb18e22c037ee29cbdeb908909ecaf
chhaya <chhaya@LAPTOP-QTOJQVJQ>
2017-07-27 22:44:05 +0530

    Assignment-2-1 Commit

---
+++ src/corejavaAssignment2_1/Assignment_2_1.java
@@ -0,0 +1,33 @@
+package corejavaAssignment2_1;
+
+public class Assignment_2_1 
+{
+
+	public static void main(String[] args)
+	{
+	     int a = 1057 ,b = 10 , c = 11300 ;
+	     
+	     int highest = 0;
+	     
+
+	// logic to display highest number among the given 3 numbers
+	     
+	        if(a > b)
+	          {
+	    	    highest = a ;
+	          }
+	        else
+	          {
+	    	    highest = b ;
+	          }
+	     
+	        if (c > highest)
+	          {
+	    	    highest = c ;
+	    	  }
+	        
+	        System.out.println("Highest of all the 3 numbers is "  + highest);
+
+	}
+
+}
